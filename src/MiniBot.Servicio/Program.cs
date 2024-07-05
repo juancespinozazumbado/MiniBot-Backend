541926950ed08d5ef1ed8073628701e7a604f0db
juancespinozazumbado <espinozajuanki@gmail.com>
2024-07-05 04:56:42 -0600

    corrige  error Cors

--- src/MiniBot.Servicio/Program.cs
+++ src/MiniBot.Servicio/Program.cs
@@ -24,6 +24,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,10 +43,14 @@ if (app.Environment.IsDevelopment())
 }
 
 
+
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAnyOrigin");
 
 app.UseAuthorization();
 
